f7c010f3f689313eab773065d2cf9f17aeb6dde7
Masaya Kataoka <ms.kataoka@gmail.com>
2019-09-04 10:16:46 +0900

    add message_filters

--- include/dwa_planner/dwa_planner.h
+++ include/dwa_planner/dwa_planner.h
@@ -6,9 +6,17 @@
 #include <dynamic_reconfigure/server.h>
 #include <usv_navigation_msgs/Path.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+// Headers in STL
+#include <memory>
 
 // Headers in this package
 #include <dwa_planner/DwaPlannerConfig.h>
+
+typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, geometry_msgs::TwistStamped> SyncPolicy;
 
 class DwaPlanner
 {
@@ -20,19 +28,25 @@ private:
     dwa_planner::DwaPlannerConfig config_;
     void twistStampedCallback(const geometry_msgs::TwistStamped::ConstPtr msg);
     void pathCallback(const usv_navigation_msgs::Path::ConstPtr msg);
+    void currentPoseCallback(const geometry_msgs::PoseStamped::ConstPtr msg);
+    void poseTwistCallback(const geometry_msgs::PoseStamped::ConstPtr pose,const geometry_msgs::TwistStamped::ConstPtr twist);
     dynamic_reconfigure::Server<dwa_planner::DwaPlannerConfig> server_;
     dynamic_reconfigure::Server<dwa_planner::DwaPlannerConfig>::CallbackType param_func_;
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
     usv_navigation_msgs::Path path_;
-    ros::Subscriber twist_stamped_sub_;
+    //ros::Subscriber twist_stamped_sub_;
     std::string twist_stamped_topic_;
     ros::Subscriber path_sub_;
     std::string path_topic_;
     ros::Publisher twist_cmd_pub_;
     std::string twist_cmd_topic_;
+    std::string current_pose_topic_;
     std::vector<double> getAngularVelList(geometry_msgs::TwistStamped twist);
     std::vector<double> getLinearVelList(geometry_msgs::TwistStamped twist);
+    std::shared_ptr<message_filters::Subscriber<geometry_msgs::PoseStamped> > pose_sub_ptr_;
+    std::shared_ptr<message_filters::Subscriber<geometry_msgs::TwistStamped> > twist_sub_ptr_;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_ptr_;
 };
 
 #endif  //DWA_PLANNER_DWA_PLANNER_H_INCLUDED
--- src/dwa_planner.cpp
+++ src/dwa_planner.cpp
@@ -10,14 +10,25 @@ DwaPlanner::DwaPlanner(ros::NodeHandle nh,ros::NodeHandle pnh)
     pnh_.param<std::string>("twist_stamped_topic", twist_stamped_topic_, ros::this_node::getName()+"/twist_stamped");
     pnh_.param<std::string>("path_topic", path_topic_, ros::this_node::getName()+"/path");
     pnh_.param<std::string>("twist_cmd_topic", twist_cmd_topic_, ros::this_node::getName()+"/twist_cmd_topic");
+    pnh_.param<std::string>("current_pose_topic", current_pose_topic_, ros::this_node::getName()+"/current_pose_topic");
     twist_cmd_pub_ = nh_.advertise<geometry_msgs::TwistStamped>(twist_cmd_topic_,1);
-    twist_stamped_sub_ = nh_.subscribe(twist_stamped_topic_,1,&DwaPlanner::twistStampedCallback,this);
+    twist_sub_ptr_ = std::make_shared<message_filters::Subscriber<geometry_msgs::TwistStamped> >(nh_,twist_stamped_topic_,10);
+    pose_sub_ptr_ = std::make_shared<message_filters::Subscriber<geometry_msgs::PoseStamped> >(nh_,current_pose_topic_,10);
+    sync_ptr_ = std::make_shared<message_filters::Synchronizer<SyncPolicy> >(SyncPolicy(10), *pose_sub_ptr_, *twist_sub_ptr_ );
+    sync_ptr_->registerCallback(boost::bind(&DwaPlanner::poseTwistCallback, this, _1, _2));
     path_sub_ = nh_.subscribe(path_topic_,1,&DwaPlanner::pathCallback,this);
 }
 
 DwaPlanner::~DwaPlanner()
 {
 
+}
+
+void DwaPlanner::poseTwistCallback(const geometry_msgs::PoseStamped::ConstPtr pose,const geometry_msgs::TwistStamped::ConstPtr twist)
+{
+    std::vector<double> angular_vel_list = getAngularVelList(*twist);
+    std::vector<double> linear_vel_list = getAngularVelList(*twist);
+    return;
 }
 
 void DwaPlanner::paramsCallback(dwa_planner::DwaPlannerConfig &config, uint32_t level)
@@ -29,13 +40,6 @@ void DwaPlanner::paramsCallback(dwa_planner::DwaPlannerConfig &config, uint32_t 
 void DwaPlanner::pathCallback(const usv_navigation_msgs::Path::ConstPtr msg)
 {
     path_ = *msg;
-    return;
-}
-
-void DwaPlanner::twistStampedCallback(const geometry_msgs::TwistStamped::ConstPtr msg)
-{
-    std::vector<double> angular_vel_list = getAngularVelList(*msg);
-    std::vector<double> linear_vel_list = getAngularVelList(*msg);
     return;
 }
 
